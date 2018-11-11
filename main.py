510cb5a3dbf258f540e944e49196885cd5f41fc8
ElenaShe000 <eshevnina@gmail.com>
2018-11-11 15:52:57 +0200

    Update main.py
    we need a structure and comments in the code. I added some, and corrected the formula for the humidity correction of the instrument

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ import math
 import numpy
 from datetime import datetime, timedelta
 
-# ###########################CONSTANTS#################################
-FILE = "IRGASON-181012_1229.dat.bz2"  # relative path
-FORMAT = "08"  # "08"|"07"
-FREQUENCY = 20
-INTERVAL = 30
+# ###########################CONFIGURATION OF THE EXPERIMENT#################################
+FILE = "IRGASON-181012_1229.dat.bz2"  # relative path to file 
+FORMAT = "08"  # "08"|"07"            # OS EC100 08.01 or EC100 07.01
+FREQUENCY = 20                        # the unpromtem output frequency, Hz
+INTERVAL = 30                         # interval for the flux calculation, min
+Sonic_azimut = 137                    # direction of the instriments, degree
+Sonic_height = 2                      # height of intrument, m
 
 
-# ############################################################
+
+# ###################### PHYSICAL COSTANTS #################################################
+R=8.3144598         #the universal gas constant, kg m2 s−2 K−1 mol−1
+
+
+
+
+
+# ############################# FUNCTIONS DECLARATION ######################################
+# ############################# READ DATA ##################################################
 
 def get_from_file(filename, file_format, freq):
     struct = {
@@ -35,24 +46,24 @@ def get_from_file(filename, file_format, freq):
                     struct["From"] = dt
                 struct["RawData"].append({
                     'DateStamp': dt,
-                    'Ux': float(firstfline[2]) if firstfline[2] else numpy.nan,
-                    'Uy': float(line[1]) if line[1] else numpy.nan,
-                    'Uz': float(line[2]) if line[2] else numpy.nan,
-                    'SonicTemperature': float(line[3]) if line[3] else numpy.nan,
-                    'SonicDiagnosticFlag': float(line[4]) if line[4] else numpy.nan,
-                    'CO2Density': float(line[5]) if line[5] else numpy.nan,
-                    'H2ODensity': float(line[6]) if line[6] else numpy.nan,
-                    'GasDiagnosticFlag': float(line[7]) if line[7] else numpy.nan,
-                    'AirTemperature': float(line[8]) if line[8] else numpy.nan,
-                    'AirPressure': float(line[9]) if line[9] else numpy.nan,
-                    'CO2SignalStrengthNominally': float(line[10]) if line[10] else numpy.nan,
-                    'H2OSignalStrengthNominally': float(line[11]) if line[11] else numpy.nan,
-                    'PressureDifferential': float(line[12]) if line[12] else numpy.nan,
-                    'CO2Correct': float(line[12]) if line[12] else numpy.nan,
-                    'SourceHousingTemperature': float(line[13]) if line[13] else numpy.nan,
-                    'DetectorHousingTemperature': float(line[14]) if line[14] else numpy.nan,
-                    'CounterArbitrary': int(line[15]) if line[15] else numpy.nan,
-                    'SignatureArbitrary': line[16] if line[16] else ''
+                    'Ux': float(firstfline[2]) if firstfline[2] else numpy.nan,                   # ms-1
+                    'Uy': float(line[1]) if line[1] else numpy.nan,                               # ms-1
+                    'Uz': float(line[2]) if line[2] else numpy.nan,                               # ms-1
+                    'SonicTemperature': float(line[3]) if line[3] else numpy.nan,                 # C
+                    'SonicDiagnosticFlag': float(line[4]) if line[4] else numpy.nan,              #
+                    'CO2Density': float(line[5]) if line[5] else numpy.nan,                       # mg m-3
+                    'H2ODensity': float(line[6]) if line[6] else numpy.nan,                       # g m-3
+                    'GasDiagnosticFlag': float(line[7]) if line[7] else numpy.nan,                #
+                    'AirTemperature': float(line[8]) if line[8] else numpy.nan,                   # C
+                    'AirPressure': float(line[9]) if line[9] else numpy.nan,                      # kPa 
+                    'CO2SignalStrengthNominally': float(line[10]) if line[10] else numpy.nan,     #
+                    'H2OSignalStrengthNominally': float(line[11]) if line[11] else numpy.nan,     #
+                    'PressureDifferential': float(line[12]) if line[12] else numpy.nan,           # kPa
+                    'CO2Correct': float(line[12]) if line[12] else numpy.nan,                     # mg m-3
+                    'SourceHousingTemperature': float(line[13]) if line[13] else numpy.nan,       # C not needed 
+                    'DetectorHousingTemperature': float(line[14]) if line[14] else numpy.nan,     # C not needed
+                    'CounterArbitrary': int(line[15]) if line[15] else numpy.nan,                 # not needed
+                    'SignatureArbitrary': line[16] if line[16] else ''                            # not needed
                 })
                 # for testing only!
 
@@ -63,6 +74,7 @@ def get_from_file(filename, file_format, freq):
         return struct
 
 
+########################### SPLITTING INTO 30 MIN INTERVALS ##################################################
 def split_struct(struct, interval, freq):
     if len(struct["RawData"]) < 1:
         return False
@@ -80,20 +92,21 @@ def split_struct(struct, interval, freq):
             return False
     return struct
 
-
+################################# FILTERING: BY A NUMBER OF MEASUREMENTS ##################################### 
 def filter_struct(struct, interval, freq):
     struct["Filtered"] = [x for x in struct["Filtered"] if len(x) >= freq * interval * 60 / 2]
     return struct
 
 
+############### SONIC TEMPERATURE HUMIDITY CORRECTION: Kaimal and Gaynor (1991) ###############################
 def add_t_corrected(x):
     x.update({"TemperatureC": (x["SonicTemperature"] + 273.15) /
-                              (1 + 0.32 * x['H2ODensity'] * 8.3143 * 0.001 * (x["SonicTemperature"] + 273.15) /
-                               18.02 * x['AirPressure'])
+                              ((1 + 0.32 * x['H2ODensity']*1000 * R * (x["SonicTemperature"] + 273.15)) /
+                               18.02 * x['AirPressure']*1000)
               })
     return x
 
-
+########################### FILTERING: BY DAGNOSTIC FLAGS ####################################################
 def diagnostic_filter(struct):
     for index in range(len(struct["Filtered"])):
         struct["Filtered"][index] = [x for x in struct["Filtered"][index]
@@ -104,6 +117,8 @@ def diagnostic_filter(struct):
                                      ]
     return struct
 
+
+######################### MAIN CODE ############################################################################
 
 print("Get from file...")
 structure = get_from_file(FILE, FORMAT, FREQUENCY)
@@ -127,3 +142,4 @@ structure = filter_struct(structure, INTERVAL, FREQUENCY)
 
 data_q_c = len(structure["Filtered"]) / (24 * 60 / INTERVAL / 100)
 print("Data Quality Control = {} % ".format(data_q_c))
+
